fe0c744fc6d895e03dddd26971f05ce1fe4f6807
Asura <jose254ernesto1@gmail.com>
2025-05-30 11:40:29 -0400

    vitest

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+/// <reference types="vitest"/>
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -6,4 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: 'https://asuradev1.github.io/',
+  test: {
+    environment: "jsdom"
+  }
 })
